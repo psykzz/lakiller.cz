84cc8896a4673210e09888ff4d1789472abfd187
Dominik Panic <dominikpanic99@gmail.com>
2019-04-26 14:15:16 +0200

    Improves database connection error handling

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import mysql.connector
 from os import path
 
 config = configparser.ConfigParser()
+
 if path.isfile('production.ini'):
 	config.read('production.ini')
 else:
 	config.read('config.ini')
+
 dbconfig = config['Database']
 
 dbusername = dbconfig['dbusername']
@@ -19,28 +21,45 @@ dbname = dbconfig['dbname']
 database = None
 cursor = None
 
-try:
-	database = mysql.connector.connect(user = dbusername, password = dbpassword, host = dbhost, port = dbport, database = dbname)
-	cursor = database.cursor(buffered = True)
-except:
-	pass
-
 
 def handle_connection():
-	if not database:
+	global database
+	global cursor
+
+	if database is None:
+		try_connect()
+
+	if database is None:
 		return False
+
 	status = database.is_connected()
-	if not status:
-		database.reconnect()
-	status = database.is_connected()
-	if not status:
-		return False
-	else:
+	if status:
 		return True
-		
+	else:
+		try_connect()
+
+	status = database.is_connected()
+	if status:
+		return True
+	else:
+		return False
+
+
+def try_connect():
+	global database
+	global cursor
+
+	try:
+		database = mysql.connector.connect(user = dbusername, password = dbpassword, host = dbhost, port = dbport, database = dbname)
+		cursor = database.cursor(buffered = True)
+	
+	except:
+		pass
+
 
 def connection_error():
 	return template('template/error')
+
 
 
 @route("/")
@@ -49,26 +68,37 @@ def index(cursor = cursor):
 
 
 @route("/poll", method = "GET")
-def poll(cursor = cursor):
+def poll():
+	global cursor
+
 	if not handle_connection():
 		return connection_error()
+
 	offset = request.query.offset
+
 	try:
 		offset = int(offset)
 		return template('template/poll', cursor = cursor, offset = offset)
 	except:
 		return template('template/poll', cursor = cursor, offset = 0)
 
+
 @route("/poll/<pollid:int>")
-def pollid(cursor = cursor, pollid = None):
+def pollid(pollid = None):
+	global cursor
+
 	if not handle_connection():
 		return connection_error()
+
 	return template('template/pollid', cursor = cursor, pollid = pollid)
 
 
 @route('/static/<filename:path>')
 def send_static(filename):
 	return static_file(filename, root='static/')
+
+
+try_connect()
 
 
 if __name__ == "__main__":

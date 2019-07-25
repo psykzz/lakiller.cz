64fb586771ce276131d78fefa5ac5e074f1bfdd2
Dominik Panic <dominikpanic99@gmail.com>
2019-07-25 14:27:14 +0200

    Converts functional approach into an OOP one

--- app.py
+++ app.py
@@ -1,46 +1,34 @@
 from bottle import default_app, run, route, template, static_file, request
 import src.base
 
-database = None
-cursor = None
+statbus = src.base.Statbus()
 
 @route("/")
-def index(cursor = cursor):
-	 return src.base.generate_template(template('template/index', cursor = cursor))
+def index():
+	 return statbus.generate_template(template('template/index', cursor = statbus.cursor))
 
 
 @route("/poll", method = "GET")
 def poll():
-	global cursor
-
-	if not src.base.handle_connection():
-		return src.base.connection_error()
+	if not statbus.handle_connection():
+		return statbus.connection_error()
 
 	offset = request.query.offset
 
-	return src.base.generate_template(template('template/poll', cursor = cursor, offset = offset))
+	return statbus.generate_template(template('template/poll', cursor = statbus.cursor, offset = offset))
 
 
 @route("/poll/<pollid:int>")
 def pollid(pollid = None):
-	global cursor
+	if not statbus.handle_connection():
+		return statbus.connection_error()
 
-	if not src.base.handle_connection():
-		return src.base.connection_error()
-
-	return src.base.generate_template(template('template/pollid', cursor = cursor, pollid = pollid))
+	return statbus.generate_template(template('template/pollid', cursor = statbus.cursor, pollid = pollid))
 
 
 @route('/static/<filename:path>')
 def send_static(filename):
 	return static_file(filename, root = 'static/')
-
-
-src.base.try_connect()
-
-
-database = src.base.database
-cursor = src.base.cursor
 
 
 if __name__ == "__main__":
--- src/base.py
+++ src/base.py
@@ -3,62 +3,62 @@ from configparser import ConfigParser
 from bottle import template
 from os import path
 
-database = None
-cursor = None
+class Statbus():
+	database = None
+	cursor = None
 
-def handle_connection():
-	global database
-	global cursor
 
-	if database is None:
-		try_connect()
+	def __init__(self):
+		self.try_connect()
 
-	if database is None:
-		return False
 
-	status = database.is_connected()
-	if status:
-		return True
-	else:
-		try_connect()
+	def try_connect(self):
+		config = ConfigParser()
 
-	status = database.is_connected()
-	if status:
-		return True
-	else:
-		return False
+		if path.isfile('config/production.ini'):
+			config.read('config/production.ini')
+		else:
+			config.read('config/config.ini')
+
+		dbconfig = config['Database']
+
+		dbusername = dbconfig['dbusername']
+		dbpassword = dbconfig['dbpassword']
+		dbhost = dbconfig['dbhost']
+		dbport = dbconfig['dbport']
+		dbname = dbconfig['dbname']
+
+		try:
+			self.database = connect(user = dbusername, password = dbpassword, host = dbhost, port = dbport, database = dbname)
+			self.cursor = self.database.cursor(buffered = True)
+			return True
+		except:
+			return False
+
+      
+	def handle_connection(self):
+		if self.database is None:
+			self.try_connect()
+
+		if self.database is None:
+			return False
+
+		status = self.database.is_connected()
+		if status:
+			return True
+		else:
+			try_connect()
+
+		status = self.database.is_connected()
+		if status:
+			return True
+		else:
+			return False
  
 
-def try_connect():
-	global database
-	global cursor
-
-	config = ConfigParser()
-
-	if path.isfile('config/production.ini'):
-		config.read('config/production.ini')
-	else:
-		config.read('config/config.ini')
-
-	dbconfig = config['Database']
-
-	dbusername = dbconfig['dbusername']
-	dbpassword = dbconfig['dbpassword']
-	dbhost = dbconfig['dbhost']
-	dbport = dbconfig['dbport']
-	dbname = dbconfig['dbname']
-
-	try:
-		database = connect(user = dbusername, password = dbpassword, host = dbhost, port = dbport, database = dbname)
-		cursor = database.cursor(buffered = True)
-	
-	except:
-		pass
+	def connection_error(self):
+		return self.generate_template(template('template/error'))
 
 
-def connection_error():
-	return generate_template(template('template/error'))
-
-
-def generate_template(templ):
-	return template('template/header') + templ + template('template/footer')
+	def generate_template(self, templ):
+		return template('template/header') + templ + template('template/footer')

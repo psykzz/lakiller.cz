fb02fdc1cf2b8995dba8ae79b9d1abe376b6168a
PsyKzz <matt.daemon660@gmail.com>
2019-08-25 01:46:59 +0100

    Unused import

--- statbus/database.py
+++ statbus/database.py
@@ -1,8 +1,6 @@
 from playhouse.mysql_ext import MySQLConnectorDatabase
 from playhouse.flask_utils import FlaskDB
 from peewee import *
-from .config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
-
 
 # Setup the mysql connection and flaskdb wrapper.
 db_wrapper = FlaskDB()

41acfbcc65e82fdb35cb6e6fb7f2c999c8d6807d
Dominik Panic <dominikpanic99@gmail.com>
2019-09-08 17:36:31 +0200

    Winrate base

--- statbus/views/rounds.py
+++ statbus/views/rounds.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, abort
 from playhouse.flask_utils import PaginatedQuery
 from statbus.database import Round
+from datetime import datetime, timedelta
 
 
 bp = Blueprint("rounds", __name__)
@@ -9,12 +10,7 @@ bp = Blueprint("rounds", __name__)
 @bp.route("/round")
 def roundmain():	
 	rounds = (
-		Round.select(
-			Round.id,
-			Round.game_mode,
-			Round.game_mode_result,
-			Round.map_name,
-		)
+		Round.select(Round.id)
 		.order_by(Round.id.desc())
 	)
 
@@ -30,3 +26,20 @@ def roundid(round_id):
 		abort(404)
 
 	return render_template("rounds/round_info.html", round_info = round_info)
+
+
+@bp.route("/round/winrates")
+def winrates():
+	rounds = (
+		Round.select(
+			Round.game_mode,
+			Round.game_mode_result,
+			Round.map_name
+		)
+		.order_by(Round.id.desc())
+		.where(Round.initialize_datetime > (datetime.now() - timedelta(days = 30)))
+		)
+
+	winrates = None
+
+	return render_template("rounds/winrates.html", winrates = winrates, rounds = rounds)
